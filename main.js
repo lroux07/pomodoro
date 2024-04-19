de6d17d3a546dd45d36a29978a0e9a36f1c38374
lroux07 <lroux07@edenschool.fr>
2024-04-19 15:01:10 +0200

    Css - conditions - errors

--- main.js
+++ main.js
@@ -4,28 +4,36 @@ let steps = document.querySelector('#steps');
 const send = document.querySelector('#send');
 const section = document.querySelector('#section');
 let numberArticles = 0;
+let err = document.querySelector('#err');
 
 send.addEventListener('click', () => {
-    const article = document.createElement('article');
-    section.appendChild(article);
-    const header = document.createElement('header');
-    article.appendChild(header);
-    const footer = document.createElement('footer');
-    article.appendChild(footer);
-    console.log(article);
+    if(taskName.value === "" || infos.value === "" || steps.value === "") {
+        alert('Err! Tout les champs ne sont pas valident...');
+    } else {
+        const article = document.createElement('article');
+        section.appendChild(article);
+        const header = document.createElement('header');
+        article.appendChild(header);
+        const footer = document.createElement('footer');
+        article.appendChild(footer);
+        console.log(article);
 
-    let h3 = document.createElement('h3');
-    h3.textContent = taskName.value;
-    header.appendChild(h3);
-    let p = document.createElement('p');
-    p.textContent = infos.value;
-    header.appendChild(p);
+        let h3 = document.createElement('h3');
+        h3.textContent = taskName.value;
+        header.appendChild(h3);
+        let p = document.createElement('p');
+        p.textContent = infos.value;
+        header.appendChild(p);
 
-    numberArticles++;
+        numberArticles++;
+    }
 
-    if(numberArticles < 35) {
+    if(numberArticles < 33) {
         send.disabled = false;
+        err.classList.remove('err');
     } else {
         send.disabled = true;
+        err.textContent = "Err! Vous avez atteint le nombre de tâches qui vous est attibué.";
+        err.classList.add('err');
     }
 })
